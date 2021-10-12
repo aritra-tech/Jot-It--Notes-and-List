db541569f13d72892812fe17b2c3a4acc2156c63
Aritra Das <aritrarick2002@gmail.com>
2021-10-13 01:33:52 +0530

    Splash Screen Added.

---
+++ app/src/main/java/com/geekym/notedown/Activity/Splash.java
@@ -0,0 +1,39 @@
+package com.geekym.notedown.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.geekym.notedown.MainActivity;
+import com.geekym.notedown.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread td = new Thread(){
+
+            public void run()
+            {
+                try {
+                        sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };td.start();
+    }
+}
